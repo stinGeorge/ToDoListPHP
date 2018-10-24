987fc59f7cb186ba94abb09ba0a22502b206534b
Sting Arroyo <stingarroyo@gmail.com>
2018-10-23 23:42:40 -0500

    Adding Eloquent by Composer.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $post = $_POST;
 
-var_dump($post)
+var_dump($post);
 ?>
 <!doctype html>
 <html lang="ES_es">
@@ -10,13 +10,14 @@ var_dump($post)
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP - Uso de ORM</title>
+    <title>PHP - Uso de ORM Eloquent</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 <body>
 <h1>Uso de ORM</h1>
 <h2>Agregar una tarea</h2>
 <form method="post">
-    <input type="text" name="task">
+    <input type="text" name="name">
     <button type="submit">Guardar</button>
 </form>
 </body>
