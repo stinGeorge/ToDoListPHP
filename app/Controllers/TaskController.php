24b0805d7b5d5c6ae1e613e0913a5a70a8f2b5a7
Sting Arroyo <stingarroyo@gmail.com>
2018-10-25 00:02:03 -0500

    Adding validators.

--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controllers;
 use App\Models\Task;
+use Respect\Validation\Validator as v;
 
 class TaskController extends BaseController{
     public function addAction($request){
+        $responseMessage = '';
+        $statusMessage = 0;
         if($request->getMethod() === 'POST'){
             $postData = $request->getParsedBody();
-            $task = new Task();
-            $task->name = $postData['name'];
-            $task->save();
+            $jobValidator = v::key('name', v::stringType()->notEmpty());
+
+            try{
+                $jobValidator->assert($postData);
+                $task = new Task();
+                $task->name = $postData['name'];
+                $task->save();
+                $responseMessage = 'Saved!!!';
+                $statusMessage = 1;
+            }catch (\Exception $e){
+                $responseMessage = $e->getMessage();
+                $statusMessage = -1;
+            }
         }
 
-        return $this->renderHTML('addTask.twig');
+        return $this->renderHTML('addTask.twig', array(
+            'responseMessage' => $responseMessage,
+            'statusMessage' => $statusMessage
+        ));
     }
 }
